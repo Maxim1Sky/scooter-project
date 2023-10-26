8acc3dbacd985db5537fbfb65b23fd61649b9892
Maxim Toma <maxim.toma@sky.uk>
2023-10-26 15:59:51 +0100

    Added some code from Codio for Scooter class and test

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,14 @@
-class Scooter{
+class Scooter {
   // scooter code here
+
+  async charge() {
+    console.log("Starting charge");
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // wait 2 seconds
+    this.charge = 100;
+
+    console.log("Charge complete");
+  }
 }
 
-
-module.exports = Scooter
+module.exports = Scooter;
